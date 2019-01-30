694ca0d6edbfce58f9a442f8db540c3797cded36
Chris <christophermyoder@gmail.com>
2019-01-30 21:24:16 +0000

    last commit.

--- main.js
+++ main.js
@@ -17,9 +17,7 @@ class Book {
         localStorage.setItem('books', JSON.stringify(books));
     }
 
-    getBooks(){
-        let books = JSON.parse(localStorage.getItem('books'));
-    }
+
 }
 
 class UI {
@@ -28,13 +26,16 @@ class UI {
         const bookList = document.getElementById("bookList");
         const frag = document.createDocumentFragment();
 
+
+
+        bookList.innerHTML = '';
         books.forEach( book => {
             const row = document.createElement('tr');
             row.innerHTML = `
-                <td>${book.title}</td>
+                <td class="bookTitle">${book.title}</td>
                 <td>${book.author}</td>
                 <td>${book.owner}</td>
-                <td><a href="" class="delete">X</td>`;
+                <td class="delete">X</td>`;
             frag.appendChild(row);
         });
 
@@ -42,14 +43,24 @@ class UI {
     }
 
     showAlert(message, className){
-        alert(`${this.className}: ${this.message}`);
+        alert(`${className}: ${message}`);
     }
 
-    delete(target){
-        let books = JSON.parse(localStorage.getItem('books')); //should give an arr
-        let targetIndex = books.indexOf(target);
-        books.splice(targetIndex, 1);
-        target.parentElement.parentElement.remove();
+    deleteBook(target){
+      //broken
+
+      if(target.className === 'delete'){
+        let books = JSON.parse(localStorage.getItem('books'));
+
+          books.forEach(book => {
+            if(book['title'] === target.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML){
+              books.splice(book, 1);
+              localStorage.setItem('books', JSON.stringify(books));
+            }
+          });
+
+      }
+
     }
 
     clearAddBookFields(){
@@ -68,27 +79,124 @@ class UI {
     }
 }
 
+// var input, filter, table, tr, td, i, txtValue;
+//   input = document.getElementById("myInput");
+//   filter = input.value.toUpperCase();
+//   table = document.getElementById("myTable");
+//   tr = table.getElementsByTagName("tr");
 
+//   // Loop through all table rows, and hide those who don't match the search query
+//   for (i = 0; i < tr.length; i++) {
+//     td = tr[i].getElementsByTagName("td")[0];
+//     if (td) {
+//       txtValue = td.textContent || td.innerText;
+//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//         tr[i].style.display = "";
+//       } else {
+//         tr[i].style.display = "none";
+//       }
+//     }
+//   }
+// }
 
+class Search{
+  titleSearch(){
+    let input = document.getElementById('sTitleInput');
+    let filter = input.value.toUpperCase();
+    let table = document.getElementById('bookTable');
+    let tr = table.getElementsByTagName('tr');
+    let textValue;
+
+    for(let i = 0; i < tr.length; i++){
+      td = tr[i].getElementsByTagName('td')[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if(txtValue.toUpperCase().indexOf(filter) > -1){
+          tr[i].style.display='';
+        }
+        else{
+          tr[i].style.display = 'none';
+        }
+      }
+    }
+  }
+
+  authorSearch(){
+    let input = document.getElementById('sAuthorInput');
+    let filter = input.value.toUpperCase();
+    let table = document.getElementById('bookTable');
+    let tr = table.getElementsByTagName('tr');
+    let textValue;
+
+    for(let i = 0; i < tr.length; i++){
+      td = tr[i].getElementsByTagName('td')[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if(txtValue.toUpperCase().indexOf(filter) > -1){
+          tr[i].style.display='';
+        }
+        else{
+          tr[i].style.display = 'none';
+        }
+      }
+    }
+  }
+
+  ownerSearch(){
+    let input = document.getElementById('sOwnerInput');
+    let filter = input.value.toUpperCase();
+    let table = document.getElementById('bookTable');
+    let tr = table.getElementsByTagName('tr');
+    let textValue;
+
+    for(let i = 0; i < tr.length; i++){
+      td = tr[i].getElementsByTagName('td')[2];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if(txtValue.toUpperCase().indexOf(filter) > -1){
+          tr[i].style.display='';
+        }
+        else{
+          tr[i].style.display = 'none';
+        }
+      }
+    }
+  }
+}
 
 function setStorageAndTableData(){
     if(localStorage.hasOwnProperty('books') === false) {
         localStorage.setItem('books', '[]');
     }
     else{
-        let ui = new UI;
+        let ui = new UI();
         ui.render();
     }
 }
 
-document.getElementById('addSubmit').addEventListener('submit', function() {
+document.getElementById('addSubmitBtn').addEventListener('click', function(e){
     const title = document.getElementById('aTitleInput').value;
     const author = document.getElementById('aAuthorInput').value;
     const owner = document.getElementById('aOwnerInput').value;
 
     const book = new Book(title, author, owner);
-    const ui = new UI;
+    const ui = new UI();
 
-    books.addBook(title, author, owner);
-    ui.render();
+    if(title === '' || author === '' || owner === ''){
+      ui.showAlert('Please fill in all fields', 'Error');
+    }
+    else{
+      book.addBook(title, author, owner);
+      ui.render();
+      ui.clearAddBookFields();
+    }
+});
+
+
+
+document.getElementById('bookList').addEventListener('click', function(e){
+  const ui = new UI();
+
+  ui.deleteBook(e.target);
+  ui.render();
 })
